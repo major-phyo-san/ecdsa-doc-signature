fb62ce809dd18694424ae6ce2bdc2214eb8b3e32
Phyo Kyaw San <phyojupiter9@gmail.com>
2025-08-28 12:39:47 +0630

    Aug 28 commit

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ class MainWindow(QTabWidget):
         # Add tabs
         self.addTab(self.page1, "Key Pair Generation")
         self.addTab(self.page2, "Sign Documents")
-        self.addTab(self.page3, "Verify Documents")
+        # self.addTab(self.page3, "Verify Documents")
 
 def apply_stylesheet(app):
     app.setStyleSheet("""
